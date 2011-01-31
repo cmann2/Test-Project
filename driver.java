6bd45098d4e0a423ca0c4fca4cfed335a955ba95
cmann2 <mann.chr2@uwlax.edu>
2011-01-31 15:47:11 -0800

    Added another stupid line to the program.

--- driver.java
+++ driver.java
@@ -4,5 +4,6 @@ public static void main(String[] args){
 	System.out.println("Hello World");
         System.out.println("Updated online, pull me down for a new message!");
         System.out.println("Fuck You World! hahahahaha");
+        System.out.println("This is the dumbest program ever!");
 }
 }
